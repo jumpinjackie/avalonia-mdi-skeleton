40b714051e5a2bf281705aae805c79db2cbc30ae
Jackie Ng <jumpinjackie@gmail.com>
2024-02-22 20:00:16 +1100

    Add skeleton menu with sidebar and main region

---
+++ src/Maestro/Views/SidebarView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Maestro.Views
+{
+    public partial class SidebarView : UserControl
+    {
+        public SidebarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
