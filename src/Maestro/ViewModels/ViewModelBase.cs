57dd4dca76f2a27109f990e0f0d85108b30c990c
Jackie Ng <jumpinjackie@gmail.com>
2024-02-22 18:47:23 +1100

    Initial new project template + gitignore

---
+++ src/Maestro/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Maestro.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ src/Maestro/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Maestro.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
