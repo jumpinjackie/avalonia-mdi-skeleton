c77ac0efa5fb9b60fcfad69844d21fc219f492d8
Jackie Ng <jumpinjackie@gmail.com>
2024-02-22 22:53:04 +1100

    Implement basic databound tab control

--- src/Maestro/ViewModels/MainViewModel.cs
+++ src/Maestro/ViewModels/MainViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace Maestro.ViewModels;
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace Maestro.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to MapGuide Maestro!";
+    public ObservableCollection<ResourceContentViewModel> OpenResources { get; } = new ObservableCollection<ResourceContentViewModel>();
+
+    private int _counter = 0;
+
+    [RelayCommand]
+    private void OpenResource()
+    {
+        new ResourceContentViewModel(this.OpenResources)
+        {
+            Title = "New Resource " + (_counter++),
+            Text = $"Content for (New Resource {_counter})"
+        }.Add();
+    }
 }
---
+++ src/Maestro/ViewModels/ResourceContentViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace Maestro.ViewModels;
+
+public partial class ResourceContentViewModel : ViewModelBase
+{
+    readonly ObservableCollection<ResourceContentViewModel> _parentCollection;
+
+    public ResourceContentViewModel(ObservableCollection<ResourceContentViewModel> parentCollection)
+    {
+        _parentCollection = parentCollection;
+    }
+
+    internal void Add()
+    {
+        _parentCollection.Add(this);
+    }
+
+    [ObservableProperty]
+    private string? title;
+
+    [ObservableProperty]
+    private string? text;
+
+    [RelayCommand]
+    private void Close()
+    {
+        _parentCollection.Remove(this);
+    }
+}
